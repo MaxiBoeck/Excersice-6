fa1ccb85164747ba12a2dc676261aee1552c4adb
Maxi <maxi.boeck.1995@gmail.com>
2023-07-23 13:45:19 +0200

    Starting beta testing directly from github

--- Tests/Tests.R
+++ Tests/Tests.R
@@ -1,20 +1,18 @@
 
 library(testthat) # package for testing
 devtools::install() # Load package
-#library() # To check if package is installed
-
 
 # Test clean_text function
-test_that("clean_text entfernt unnötige Zeichen und konvertiert in Kleinbuchstaben", {
+test_that("clean_text removes unnessesery text and converts in lowercase", {
   # example text
-  text <- "Hallo! Dies ist ein Beispieltext 123."
+  text <- "Hello! This is an example text 123"
   # use clean_text function
   cleaned_text <- clean_text(text)
   # check result
-  expect_equal(cleaned_text, "hallo dies ist ein beispieltext")
+  expect_equal(cleaned_text, "hello this is an example text")
 })
 
-# Test remove_stopwords function
+# Test remove_stopwords function -English test version
 test_that("remove_stopwords removes all kind of stop words", {
   # exapmle text
   text <- "I love to explore new places and try different cuisines. Traveling allows me to experience diverse cultures and broaden my perspective. It's always exciting to embark on new adventures and create lasting memories. "
@@ -25,7 +23,7 @@ test_that("remove_stopwords removes all kind of stop words", {
 })
 
 
-# Test remove_stopwords function
+# Test remove_stopwords function -German test version
 test_that("remove_stopwords removes all kind of stop words", {
   #exapmle text
   text <- "Ich liebe es, neue Orte zu erkunden und verschiedene Küchen auszuprobieren. Reisen ermöglicht es mir, vielfältige Kulturen zu erleben und meinen Horizont zu erweitern. Es ist immer aufregend, sich auf neue Abenteuer einzulassen und bleibende Erinnerungen zu schaffen."
@@ -48,7 +46,7 @@ test_that("count_words counts all words in the text", {
 
 
 # Test all three functions
-test_that("count_words counts all words in the text", {
+test_that("removes digits ", {
   #exapmle text
   text <- "I love explore new places try different cuisines. Traveling allows me experience diverse cultures broaden my perspective. It's always exciting embark on new adventures create lasting memories."
   # use count_words function
@@ -57,6 +55,3 @@ test_that("count_words counts all words in the text", {
   expect_equal(text_count, 27)
 })
 
-
-# for automated testing
-testthat::test_file("C:/Users/MaxiB/Desktop/FortStat Abgabe/Excersise Sheet 6/Excersice-6/Tests/tests.R")
